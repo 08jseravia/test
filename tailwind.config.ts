a11529ec5bfd489d0286805541674ddbaa389494
Lukurishi <112971446+Lukurishi@users.noreply.github.com>
2025-02-11 16:08:58 -0600

    fixed screen size affecting element positions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        xl2: "1600px",
+        xl2: "1720px",
       },
     },
   },
