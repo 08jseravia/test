6512845ab4d5be64595e0e60edf9834ecdddf1c2
Lukurishi <fehrfaber@gmail.com>
2025-02-10 21:20:36 -0600

    fixed sqf to f2

--- app/components/room.js
+++ app/components/room.js
@@ -15,7 +15,7 @@ export const roomData = [
         ],
         desc: "Estas suites cuentan con impresionantes vistas al mar Caribe y se dividen en dos partes: una zona de dormitorio de tamaño king y una zona de estar con sofá, mesa de comedor y 2 Smart TV, una amplia terraza que conduce a una piscina infinita. El cuarto de baño acristalado cuenta con bañera exenta y ducha de efecto lluvia.",
         person: 2,
-        roomSize: "614 sqf | 57 m2",
+        roomSize: "614 ft² | 57 m²",
         prices: {
           high: 10005,
           low: 9017,
@@ -33,7 +33,7 @@ export const roomData = [
         ],
         desc: "Estas suites ubicadas en la planta baja cuentan con cama tamaño King, área de estar con sofá y mesa lateral. El baño con loseta de vidrio cuenta con tina independiente y ducha con efecto de lluvia. La terraza tiene acceso a la piscina principal del hotel y está equipada con mesa, dos sillas y dos camastros ubicados en la parte poco profunda de la piscina.",
         person: 2,
-        roomSize: "463 sqf | 43 m2",
+        roomSize: "463 ft² | 43 m²",
         prices: {
           high: 8446,
           low: 7367,
@@ -55,7 +55,7 @@ export const roomData = [
         ],
         desc: "Estas cómodas y bien equipadas suites están ubicadas en la planta baja y ofrecen cama tamaño King, área de estar con sofá y mesa lateral. La terraza tiene acceso a la piscina principal y cuenta con mesa, dos sillas y dos camastros ubicados en la parte poco profunda de la piscina.",
         person: 2,
-        roomSize: "452 sqf | 42 m2",
+        roomSize: "452 ft² | 42 m²",
         prices: {
           high: 8671,
           low: 7690,
@@ -81,7 +81,7 @@ export const roomData = [
         ],
         desc: "Situadas en el centro de la 5ª (última) planta de la propiedad, las espectaculares suites Coral Reefer ofrecen el máximo lujo en un entorno realmente impresionante con unas vistas preciosas al mar. Al entrar en la impresionante sala de estar, los huéspedes descubrirán 3.200 pies cuadrados de impresionantes zonas de estar y comedor interiores y exteriores con un concepto abierto de sala de estar y comedor con capacidad para 8 personas y una zona de bar.",
         person: 4,
-        roomSize: "1780 sqf | 165 m2",
+        roomSize: "1780 ft² | 165 m²",
         prices: {
           high: 14348,
           low: 13557,
@@ -112,7 +112,7 @@ export const roomData = [
         ],
         desc: "Esta suite exclusiva frente al mar cuenta con king-size, zona de estar totalmente amueblada y sofá de diseño, Smart TV e impresionantes vistas desde la habitación y el balcón. El cuarto de baño, que se abre a la sala de estar, tiene lavabos para él y para ella, una bañera independiente y dos duchas efecto lluvia, así como un vestidor.",
         person: 2,
-        roomSize: "678 sqf | 63 m2",
+        roomSize: "678 ft² | 63 m²",
         prices: {
           high: 18978,
           low: 16889,
@@ -147,7 +147,7 @@ export const roomData = [
         img: ["assets/images/room/luxury-paradise-suite-room.webp"],
         desc: "Estas suites ofrecen cama tamaño King, área de estar con sofá y mesa lateral. El baño cuenta con loseta de vidrio, tina independiente y ducha con efecto de lluvia. La terraza cuenta con una mesa y dos sillas. Disponibles suites que conectan. Estas suites están ubicadas en el segundo, tercer y cuarto piso del hotel.",
         person: 2,
-        roomSize: "463 sqf | 43 m2",
+        roomSize: "463 ft² | 43 m²",
         prices: {
           high: 6428,
           low: 5843,
@@ -168,7 +168,7 @@ export const roomData = [
         ],
         desc: "Estas cómodas y bien equipadas suites ofrecen cama tamaño King, área de estar con sofá y mesa lateral. La terraza cuenta con mesa y dos sillas.",
         person: 2,
-        roomSize: "452 sqf | 42 m2",
+        roomSize: "452 ft² | 42 m²",
         prices: {
           high: 6118,
           low: 5550,
@@ -184,7 +184,7 @@ export const roomData = [
         img: ["assets/images/room/queen-paradise-room-room.webp"],
         desc: "Estas suites están ubicadas en el segundo, tercer y cuarto piso del hotel y ofrecen dos camas tamaño Queen y una mesa para dos personas. La terraza cuenta con mesa y dos sillas. Disponibles suites que conectan.",
         person: 3,
-        roomSize: "452 sqf | 42 m2",
+        roomSize: "452 ft² | 42 m²",
         prices: {
           high: 6179,
           low: 5627,
@@ -200,7 +200,7 @@ export const roomData = [
         img: ["assets/images/room/beachfront-honeymoon-suite-room.webp"],
         desc: "Esta suite con vista al mar cuenta con una cama tamaño King, área de estar completamente equipada sofá de diseño, smart TV e impresionante vista del Mar Caribe desde esta área y del balcón. El baño, que abre hacia el área de estar, cuenta con lavamanos para él y ella, tina independiente y dos regaderas tipo lluvia, al igual que vestidor.",
         person: 2,
-        roomSize: "678 sqf | 63 m2",
+        roomSize: "678 ft² | 63 m²",
         prices: {
           high: 10155,
           low: 9370,
@@ -219,7 +219,7 @@ export const roomData = [
         ],
         desc: "Ubicadas en la parte central del cuarto piso, estas suites cuentan con fantásticas vistas del mar y de la propiedad. Estas cómodas y bien equipadas suites ofrecen una cama tamaño King, área de estar y escritorio. Los baños con loseta de vidrio cuentan con tina independiente y ducha con efecto de lluvia. Los huéspedes pueden tener acceso directo a la piscina infinity directo desde su terraza.",
         person: 2,
-        roomSize: "463 sqf | 43m2",
+        roomSize: "463 ft² | 43m²",
         prices: {
           high: 9809,
           low: 8207,
@@ -241,7 +241,7 @@ export const roomData = [
         ],
         desc: "Estas suites cuentan con magníficas vistas a la playa. Las suites están divididas en dos partes: una zona de dormitorio con cama king-size y una zona de estar con sofá, mesa de comedor y Smart TV, una terraza amueblada con mesa, sillas y sofá cama. El cuarto de baño acristalado cuenta con bañera exenta y ducha de efecto lluvia.",
         person: 2,
-        roomSize: "614 sqf | 57 m2",
+        roomSize: "614 ft² | 57 m²",
         prices: {
           high: 9285,
           low: 8207,
@@ -261,7 +261,7 @@ export const roomData = [
         ],
         desc: "Ubicadas en la parte central del quinto piso, estas suites cuentan con espléndidas vistas del mar y de la propiedad. Estas cómodas y bien equipadas suites ofrecen una cama tamaño King, área de estar con sofá y mesa lateral. Los baños con loseta de vidrio cuentan con tina independiente y ducha con efecto de lluvia. Las suites cuentan con terraza privada que permite tener vistas panorámicas del Mar Caribe.",
         person: 2,
-        roomSize: "474 sqf | 44 m2",
+        roomSize: "474 ft² | 44 m²",
         prices: {
           high: 7511,
           low: 6969,
@@ -277,7 +277,7 @@ export const roomData = [
         img: ["assets/images/room/island-oasis-suite-rooms.jpg"],
         desc: "Ubicada en la planta baja con vista a los exuberantes jardines, estas suites cuidadosamente diseñadas ofrecen cama tamaño King, área de estar con sofá y mesa lateral. La terraza tiene acceso a un oasis de jardín sereno que cuenta con una hamaca, área de descanso y una pared verde para agregarle un toque zen.",
         person: 2,
-        roomSize: "452 sqf | 42 m2",
+        roomSize: "452 ft² | 42 m²",
         prices: {
           high: 6694,
           low: 6034,
